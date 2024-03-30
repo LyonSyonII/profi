62df38aedf65131005d1fe37c23a72289badbe7b
lyonsyonii <liam@garriga.dev>
2024-03-30 19:43:47 +0100

    add flake.nix for better DX

---
+++ flake.nix
@@ -0,0 +1,53 @@
+{
+  description = "Rust Dev Flake Shell";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs = { self, nixpkgs, ... }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        inherit system;
+      };
+    in
+    with pkgs;
+    {
+      devShells.${system}.default = mkShell {
+        buildInputs = with pkgs; [
+          clang
+          # Replace llvmPackages with llvmPackages_X, where X is the latest LLVM version (at the time of writing, 16)
+          llvmPackages.bintools
+          rustup
+          sccache
+          valgrind
+        ];
+        # RUSTC_VERSION = overrides.toolchain.channel;
+        # https://github.com/rust-lang/rust-bindgen#environment-variables
+        LIBCLANG_PATH = pkgs.lib.makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+        shellHook = ''
+          export PATH=$PATH:''${CARGO_HOME:-~/.cargo}/bin
+          export PATH=$PATH:''${RUSTUP_HOME:-~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/
+        '';
+        # Add precompiled library to rustc search path
+        RUSTFLAGS = (builtins.map (a: ''-L ${a}/lib'') [
+          # add libraries here (e.g. pkgs.libvmi)
+        ]);
+        # LD_LIBRARY_PATH = libPath;
+        # Add glibc, clang, glib, and other headers to bindgen search path
+        BINDGEN_EXTRA_CLANG_ARGS =
+          # Includes normal include path
+          (builtins.map (a: ''-I"${a}/include"'') [
+            # add dev libraries here (e.g. pkgs.libvmi.dev)
+            pkgs.glibc.dev
+          ])
+          # Includes with special directory paths
+          ++ [
+            ''-I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+            ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+            ''-I${pkgs.glib.out}/lib/glib-2.0/include/''
+          ];
+      };
+    };
+}
