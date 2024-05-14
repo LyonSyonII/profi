5a0d8c59f52ecafd1849ce256b78c5a31719345f
lyonsyonii <liam@garriga.dev>
2024-05-14 15:36:24 +0200

    add manual implementation to rayon example

--- profi/examples/rayon.rs
+++ profi/examples/rayon.rs
@@ -5,12 +5,18 @@ fn main() {
     
     // Benchmark different implementations of adding up the numbers in a sequence.
     // Collect the range in a Vec to avoid optimizations.
-    const N: usize = 1 << 32;
-    let range = (0..N).collect::<Vec<_>>();
+    type Int = u32;
+    type AtomicInt = std::sync::atomic::AtomicU32;
+    const N: Int = Int::MAX;
+
+    let range = { 
+        profi::prof!("range creation");
+        (0..N).collect::<Vec<_>>() 
+    };
 
     let result = {
         profi::prof!("sequential");
-        range.iter().sum::<usize>() 
+        range.iter().sum() 
     };
     
     let reduction = true;
@@ -19,30 +25,43 @@ fn main() {
 
     if reduction {
         profi::prof!("rayon-reduction");
-        let sum: usize = range.par_iter().copied().reduce(|| 0, |a, b| a + b);
+        let sum = range.par_iter().copied().reduce(|| 0, |a, b| a + b);
         assert_eq!(sum, result);
     }
 
     if sum {
         profi::prof!("rayon-sum");
-        let sum: usize = range.par_iter().sum();
+        let sum = range.par_iter().sum::<Int>();
         assert_eq!(sum, result);
     }
 
     if manual {
         profi::prof!("manual-blocks");
         let num_threads: usize = std::thread::available_parallelism().unwrap().into();
-        let sum = std::sync::atomic::AtomicUsize::new(0);
+        let sum = AtomicInt::new(0);
         std::thread::scope(|s| {
             let sum = &sum;
-            for block in range.chunks(num_threads) {
+            let range = &range;
+            for t in 0..num_threads {
                 s.spawn(move || {
-                    sum.fetch_add(block.iter().sum(), std::sync::atomic::Ordering::Relaxed);
+                    // TODO: This causes a Deadlock
+                    profi::prof!("manual-blocks-iter");
+                    let bs = (N as usize / num_threads) as usize;
+                    let rest = (N as usize % num_threads) as usize;
+                    // Add 1 element to each thread until `rest`
+                    let start = if t < rest {
+                        bs * t + t
+                    } else {
+                        // When all extra elements have been added, adapt `start` to this extra elements
+                        bs * t + rest
+                    } as usize;
+                    let end = start + bs + (t < rest) as usize;
+                    
+                    let local_sum = range[start..end].iter().sum();
+                    sum.fetch_add(local_sum, std::sync::atomic::Ordering::AcqRel);
                 });
             }
         });
         assert_eq!(sum.into_inner(), result);
     }
-    
-    println!("{result}");
 }
